182ca883fcab21f68211b106acaae254b82e6d62
uolter <walter.traspadini@pagopa.it>
2022-09-07 12:31:41 +0200

    refactoring strapi project + database configurations

--- src/strapi/app/config/server.js
+++ src/strapi/app/config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '4b034982593c2f563a95b94c53751f52'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
