4703994eecf57fff1925d7ec8b5d0ecce377e134
uolter <walter.traspadini@pagopa.it>
2022-12-22 10:07:27 +0100

    fix: ttl not required with alias

--- src/main/15-dns_public.tf
+++ src/main/15-dns_public.tf
@@ -34,7 +34,6 @@ resource "aws_route53_record" "website" {
   zone_id = module.dns_zone[0].route53_zone_zone_id[keys(var.public_dns_zones)[0]]
   name    = ""
   type    = "A"
-  ttl     = var.dns_record_ttl
 
   alias {
     name                   = aws_cloudfront_distribution.website.domain_name
